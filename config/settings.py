8fa59c89d12cfbd3fde03451ca1fac7218d92d7c
freestudentsinghub <masha1207355@gmail.com>
2025-04-08 10:20:08 +0300

    add secret key

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = "django-insecure-olrr#vrl=gd_jbl*$n=(mb7-fo&@yulnu!zmk6tl$f3%r(zrei"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True if os.getenv('DEBUG') == "True" else False
